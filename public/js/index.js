088785a8384596e28a7c63a182ab55f23989c2b6
marcoweb <marcocarvalho.web@hotmail.com>
2020-09-22 23:36:01 +0000

    Remove

--- public/js/index.js
+++ public/js/index.js
@@ -3,14 +3,44 @@
 //     alert(livro.value);
 // }
 
+let livros = [];
+
+function exibeLista() {
+    document.querySelector("#lista").innerHTML = "";
+    for(let i = 0;i < livros.length; i++) {
+        let item = document.createElement("li");
+        item.appendChild(
+            document.createTextNode(
+                livros[i]
+            )
+        );
+        let link = document.createElement("a");
+        link.setAttribute("href", "#");
+        link.setAttribute("onclick", "removeItem(" + i  +")");
+        link.innerHTML = "Remover";
+        item.appendChild(link);
+        document.querySelector("#lista").appendChild(item);
+    }
+}
+
+function removeItem(pos) {
+    livros.splice(pos, 1);
+    exibeLista();
+}
+
 document.querySelector("button").onclick = function() {
-    let item = document.createElement("li");
+    // let item = document.createElement("li");
     // item.appendChild(
     //     document.createTextNode(
     //         document.querySelector("#livro").value
     //     )
     // );
-    item.innerHTML = document.querySelector("#livro").value;
+    // item.innerHTML = document.querySelector("#livro").value;
+    // document.querySelector("#livro").value = "";
+    // document.querySelector("#lista").appendChild(item);
+    livros.push(document.querySelector("#livro").value);
     document.querySelector("#livro").value = "";
-    document.querySelector("#lista").appendChild(item);
+    exibeLista();
 }
+
+exibeLista();
